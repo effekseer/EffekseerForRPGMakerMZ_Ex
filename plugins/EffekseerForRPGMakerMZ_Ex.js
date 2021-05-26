5b5405eea91ab1c314c5312c06991fc49c1cbb84
durswd <swda.durl@gmail.com>
2021-05-26 13:19:23 +0900

    160c

--- plugins/EffekseerForRPGMakerMZ_Ex.js
+++ plugins/EffekseerForRPGMakerMZ_Ex.js
@@ -10,7 +10,7 @@
 /*:
  * @target MZ
  * @url https://github.com/effekseer/EffekseerForRPGMakerMZ_Ex
- * @plugindesc Effekseer Extended plugin v1.60b - 1.02
+ * @plugindesc Effekseer Extended plugin v1.60c - 1.02
  * @author Effekseer
  *
  * @help
@@ -38,7 +38,7 @@
 /*:ja
  * @target MZ
  * @url https://github.com/effekseer/EffekseerForRPGMakerMZ_Ex
- * @plugindesc Effekseer 拡張プラグイン v1.60b - 1.02
+ * @plugindesc Effekseer 拡張プラグイン v1.60c - 1.02
  * @author Effekseer
  *
  * @help
