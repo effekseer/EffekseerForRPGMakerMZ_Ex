22ae099f2e519f95dc1c2d183682d259b2491fb5
durswd <swda.durl@gmail.com>
2022-09-20 11:28:48 +0900

    1.70

--- plugins/EffekseerForRPGMakerMZ_Ex.js
+++ plugins/EffekseerForRPGMakerMZ_Ex.js
@@ -10,7 +10,7 @@
 /*:
  * @target MZ
  * @url https://github.com/effekseer/EffekseerForRPGMakerMZ_Ex
- * @plugindesc Effekseer Extended plugin v1.70Beta2 - 1.04
+ * @plugindesc Effekseer Extended plugin v1.70 - 1.04
  * @author Effekseer
  *
  * @help
@@ -38,7 +38,7 @@
 /*:ja
  * @target MZ
  * @url https://github.com/effekseer/EffekseerForRPGMakerMZ_Ex
- * @plugindesc Effekseer 拡張プラグイン v1.70Beta2 - 1.04
+ * @plugindesc Effekseer 拡張プラグイン v1.70 - 1.04
  * @author Effekseer
  *
  * @help
